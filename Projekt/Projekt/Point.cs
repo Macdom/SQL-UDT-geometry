2951bffe5547a60b117612503316eb7d1a1bf356
Maciej Domagalski <ikslamabad@gmail.com>
2020-06-27 17:07:13 +0200

    Point UDT added

---
+++ Projekt/Projekt/Point.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using Microsoft.SqlServer.Server;
+
+[Serializable]
+[Microsoft.SqlServer.Server.SqlUserDefinedType(Format.Native)]
+public struct Point : INullable
+{
+    public override string ToString()
+    {
+        return "(" + this.m_x.ToString() + " " + this.m_y.ToString() + ")";
+    }
+
+    public static Point Null
+    {
+        get
+        {
+            Point h = new Point();
+            h.m_Null = true;
+            return h;
+        }
+    }
+
+    public static Point Parse(SqlString s)
+    {
+        if (s.IsNull)
+            return Null;
+        Point u = new Point();
+        //try
+        //{
+            string[] coords = s.Value.Split(' ');
+
+            u.m_x = double.Parse(coords[0]);
+            u.m_y = double.Parse(coords[1]);
+        //}
+        /*catch (Exception e)
+        {
+            throw new Exception("Nieprawidłowe współrzędne");
+        }*/
+        return u;
+    }
+
+    public bool IsNull
+    {
+        get
+        {
+            return m_Null;
+        }
+    }
+
+    public double getX()
+    {
+        return this.m_x;
+    }
+
+    public double getY()
+    {
+        return (this.m_y);
+    }
+
+    public double distance(Point a)
+    {
+        double x_a = a.getX();
+        double y_a = a.getY();
+        double x_b = this.m_x;
+        double y_b = this.m_y;
+
+        return Math.Sqrt(Math.Pow((x_b - x_a), 2) + Math.Pow((y_b - y_a), 2));
+    }
+
+    private double m_x;
+    private double m_y;
+
+    private bool m_Null;
+}
+
+
---
+++ sql/point.sql
@@ -0,0 +1,17 @@
+USE projekt
+GO
+
+DECLARE @A [dbo].[Point]
+DECLARE @B [dbo].[Point]
+
+--trzeba u¿ywaæ przecinków a nie kropek? dziwne
+
+--SET @A = '5 3'
+SET @A = '5,5 0'
+SELECT @A.ToString()
+
+SELECT @A.getX()
+SELECT @A.getY()
+
+SET @B = '-1,53 9,22'
+SELECT @A.distance(@B)
